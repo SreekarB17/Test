aca5da012b4db8338cfeeca09b4ff90f33071b8c
sahas <sahas.maduri@gmail.com>
2025-12-03 11:36:38 -0500

    implemented single threaded method to capitalize all words in a book

--- TestFile.java
+++ TestFile.java
@@ -1,8 +1,37 @@
 //Sahas Maduri and Sreekar Bheemavarapu -> 12/3/2025
+import java.io.*;
 public class TestFile
 {
 	public static void main(String [] args)
 	{
+		long start = System.nanoTime();
+
+		// read file line
+		changeFile("frankenstein.txt");
+		changeFile("mobydick.txt");
+
+		long end = System.nanoTime();
+
+		System.out.println((end-start)/100000);
+	}
+
+	public static void changeFile(String path) {
+
+		try(BufferedReader reader = new BufferedReader(new FileReader(path));
+			BufferedWriter writer = new BufferedWriter(new FileWriter("output" + path)))
+		{
+			String line = "";
+			while((line = reader.readLine()) !=null)
+			{
+				writer.write(line.toUpperCase());
+				writer.newLine();
+			}
+
+			writer.close();
+		}catch(IOException e)
+		{
+		}
+
 
 	}
 }
